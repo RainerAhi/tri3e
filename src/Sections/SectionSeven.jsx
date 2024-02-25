e3860c5548af795a8527ca44bc49a1bb795615fc
StudenxFinance <studenxfinance@gmail.com>
2024-02-25 20:32:56 +0200

    fix 4

--- src/Sections/SectionSeven.jsx
+++ src/Sections/SectionSeven.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const SectionSeven = () => {
     return (
         <section className="section seven" >
-            <h1 className="three-text" >BONDED <br/> FANDOM</h1>
+            <h1 className="three-text" >CONNECTED <br/> FANDOM</h1>
         </section>
     )
 }
