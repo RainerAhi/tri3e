82ae1be5b8f61cd598f1f44939ac29e7a0d4789b
StudenxFinance <studenxfinance@gmail.com>
2024-02-26 16:46:11 +0200

    responsive 1

--- src/Sections/SectionSeven.jsx
+++ src/Sections/SectionSeven.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const SectionSeven = () => {
     return (
         <section className="section seven" >
-            <h1 className="three-text" >CONNECTED <br/> FANDOM</h1>
+            <h1 className="seven-text" >CONNECTED <br/> FANDOM</h1>
         </section>
     )
 }
